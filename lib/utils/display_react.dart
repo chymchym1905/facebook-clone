0029333994333f1ab5f89198bf9e1419e947d14b
Danny <chungthinh5900@gmail.com>
2023-07-21 17:32:33 +0700

    add react display

--- lib/utils/display_react.dart
+++ lib/utils/display_react.dart
@@ -7,8 +7,8 @@ import '../../../utils/count_react.dart';
 
 class DisplayReact extends StatefulWidget {
   const DisplayReact({
-    super.key, 
-    required this.data, 
+    super.key,
+    required this.data,
     required this.isRevert,
   });
   final List<Reaction> data;
@@ -21,13 +21,45 @@ class _DisplayReactState extends State<DisplayReact> {
   @override
   Widget build(BuildContext context) {
     Map<String, int> listreact = {
-      "like": 0, ///0
-      "love": 0, ///1
-      "haha": 0, ///2
-      "wow": 0,  ///3
-      "sad": 0,  ///4
-      "angry": 0,///5
+      "like": 0,
+
+      ///0
+      "love": 0,
+
+      ///1
+      "haha": 0,
+
+      ///2
+      "wow": 0,
+
+      ///3
+      "sad": 0,
+
+      ///4
+      "angry": 0,
+
+      ///5
     };
+    List<String> listicon = [
+      "like",
+
+      ///0
+      "love",
+
+      ///1
+      "haha",
+
+      ///2
+      "wow",
+
+      ///3
+      "sad",
+
+      ///4
+      "angry",
+
+      ///5
+    ];
     countReact(widget.data, listreact);
     List<MapEntry<String, int>> entries = listreact.entries.toList();
     entries.sort((a, b) => b.value.compareTo(a.value));
@@ -36,40 +68,61 @@ class _DisplayReactState extends State<DisplayReact> {
     return GestureDetector(
       onTap: () {
         Navigator.of(context).pushNamed('/viewreaction',
-          arguments: ReactionPage(
-            reactions: widget.data,
-            sortedList: sortedList,
-            totalReact: totalReact,
-          )
-        );
+            arguments: ReactionPage(
+              reactions: widget.data,
+              sortedList: sortedList,
+              totalReact: totalReact,
+            ));
       },
       child: Row(
         children: [
-          if(widget.isRevert && totalReact != 0) ...[
+          if (widget.isRevert && totalReact != 0) ...[
             Text('$totalReact',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleSmall
-                  ?.copyWith(fontWeight: FontWeight.w300)),
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall
+                    ?.copyWith(fontWeight: FontWeight.w300)),
           ],
           Indexer(
             children: [
               if (sortedList.values.elementAt(1) == 0 &&
                   sortedList.values.first != 0) ...[
-                Padding(
-                  padding: EdgeInsets.only(right: widget.isRevert ? 20:7, bottom: 3),
-                  child: Image.asset(
-                    "assets/images/${sortedList.keys.first}.png",
-                    width: MediaQuery.of(context).size.width * 0.05,
-                    height: MediaQuery.of(context).size.width * 0.05,
+                Indexed(
+                  index: 2,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        right: widget.isRevert ? 0 : 7, bottom: 3, left: 7),
+                    child: Image.asset(
+                      "assets/images/${sortedList.keys.first}.png",
+                      width: MediaQuery.of(context).size.width * 0.05,
+                      height: MediaQuery.of(context).size.width * 0.05,
+                    ),
                   ),
                 ),
+                if (currUser!.name == widget.data[0].user.name &&
+                    widget.data[0].reaction != -1) ...[
+                  if (listicon[widget.data[0].reaction] !=
+                      sortedList.keys.first) ...[
+                    Indexed(
+                      index: 1,
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 23),
+                        child: Image.asset(
+                          "assets/images/${listicon[widget.data[0].reaction]}.png",
+                          width: MediaQuery.of(context).size.width * 0.05,
+                          height: MediaQuery.of(context).size.width * 0.05,
+                        ),
+                      ),
+                    )
+                  ]
+                ]
               ] else if (sortedList.values.elementAt(1) != 0 &&
                   sortedList.values.first != 0) ...[
                 Indexed(
                   index: 3,
                   child: Padding(
-                    padding: EdgeInsets.only(right: 25, bottom: 3, left:  widget.isRevert ? 5:10),
+                    padding: EdgeInsets.only(
+                        right: 22, bottom: 3, left: widget.isRevert ? 5 : 10),
                     child: Container(
                       width: MediaQuery.of(context).size.width * 0.05,
                       height: MediaQuery.of(context).size.width * 0.05,
@@ -98,7 +151,7 @@ class _DisplayReactState extends State<DisplayReact> {
                 Indexed(
                   index: 2,
                   child: Positioned(
-                    left: widget.isRevert ? MediaQuery.of(context).size.width * 0.06: MediaQuery.of(context).size.width * 0.07,
+                    left: widget.isRevert ? 21 : 26,
                     child: Padding(
                       padding: const EdgeInsets.only(bottom: 3),
                       child: Image.asset(
@@ -109,30 +162,38 @@ class _DisplayReactState extends State<DisplayReact> {
                     ),
                   ),
                 ),
-                // if(currUser == widget.data[0].user)...[
-                //   Indexed(
-                //     index: 1,
-                //     child: Positioned(
-                //       left: MediaQuery.of(context).size.width * 0.06,
-                //       child: Image.asset(
-                //         "assets/images/${sortedList.keys.elementAt(1)}.png",
-                //         width: MediaQuery.of(context).size.width * 0.05,
-                //         height: MediaQuery.of(context).size.width * 0.05,
-                //       ),
-                //     ),
-                //   )
-                // ]
+                if (currUser!.name == widget.data[0].user.name &&
+                    widget.data[0].reaction != -1) ...[
+                  if (listicon[widget.data[0].reaction] !=
+                          sortedList.keys.first &&
+                      listicon[widget.data[0].reaction] !=
+                          sortedList.keys.elementAt(1)) ...[
+                    Indexed(
+                      index: 1,
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            left: widget.isRevert ? 35.0 : 42,
+                            right: widget.isRevert ? 0 : 5),
+                        child: Image.asset(
+                          "assets/images/${listicon[widget.data[0].reaction]}.png",
+                          width: MediaQuery.of(context).size.width * 0.05,
+                          height: MediaQuery.of(context).size.width * 0.05,
+                        ),
+                      ),
+                    )
+                  ]
+                ]
               ]
             ],
           ),
-          if(!widget.isRevert) ...[
+          if (!widget.isRevert) ...[
             Text('$totalReact',
-              style: Theme.of(context)
-                  .textTheme
-                  .titleMedium
-                  ?.copyWith(fontWeight: FontWeight.w300)),
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium
+                    ?.copyWith(fontWeight: FontWeight.w300)),
             Padding(
-              padding: const EdgeInsets.only(left: 3),
+              padding: const EdgeInsets.only(left: 1),
               child: Icon(
                 size: MediaQuery.of(context).size.width * 0.05,
                 MfgLabs.right_open,
