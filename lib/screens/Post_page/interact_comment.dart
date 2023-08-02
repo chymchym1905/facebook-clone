53157a8db8e33d62aa5e5e038715647737c87b47
Danny <chungthinh5900@gmail.com>
2023-08-02 16:17:48 +0700

    interact_comment

--- lib/screens/Post_page/interact_comment.dart
+++ lib/screens/Post_page/interact_comment.dart
@@ -42,30 +42,30 @@ class _DisplayCommentState extends State<DisplayComment>
   Widget build(BuildContext context) {
     return GestureDetector(
       onLongPress: () {
-        // showModalBottomSheet<void>(
-        //     transitionAnimationController: _animationController,
-        //     useRootNavigator: true,
-        //     isScrollControlled: true,
-        //     backgroundColor: Colors.white,
-        //     shape: const RoundedRectangleBorder(
-        //         borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
-        //     context: context,
-        //     builder: (context) => const InteractComment());
-        if (widget.index3 != null) {
-          IndexComment.intdex1 = widget.index1!;
-          IndexComment.intdex2 = widget.index2!;
-          IndexComment.intdex3 = widget.index3!;
-          widget.data[widget.index1!].reply[widget.index2!].reply
-              .removeAt(widget.index3!);
-        } else if (widget.index2 != null) {
-          IndexComment.intdex1 = widget.index1!;
-          IndexComment.intdex2 = widget.index2!;
-          widget.data[widget.index1!].reply.removeAt(widget.index2!);
-        } else {
-          IndexComment.intdex1 = widget.index1!;
-          widget.data.removeAt(widget.index1!);
-        }
-        widget.reloadComment(widget.data);
+        showModalBottomSheet<void>(
+            transitionAnimationController: _animationController,
+            useRootNavigator: true,
+            isScrollControlled: true,
+            backgroundColor: Colors.white,
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(10))),
+            context: context,
+            builder: (context) => const InteractComment());
+        // if (widget.index3 != null) {
+        //   IndexComment.intdex1 = widget.index1!;
+        //   IndexComment.intdex2 = widget.index2!;
+        //   IndexComment.intdex3 = widget.index3!;
+        //   widget.data[widget.index1!].reply[widget.index2!].reply
+        //       .removeAt(widget.index3!);
+        // } else if (widget.index2 != null) {
+        //   IndexComment.intdex1 = widget.index1!;
+        //   IndexComment.intdex2 = widget.index2!;
+        //   widget.data[widget.index1!].reply.removeAt(widget.index2!);
+        // } else {
+        //   IndexComment.intdex1 = widget.index1!;
+        //   widget.data.removeAt(widget.index1!);
+        // }
+        // widget.reloadComment(widget.data);
       },
       child: Container(
         padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
@@ -124,6 +124,7 @@ class _InteractCommentState extends State<InteractComment> {
             ? const Color.fromARGB(255, 38, 38, 38)
             : whitee,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -140,15 +141,21 @@ class _InteractCommentState extends State<InteractComment> {
                 ]
               ],
             ),
-            Text("React to this comment",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(color: Colors.grey)),
+            Center(
+              child: Text("React to this comment",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(color: Colors.grey)),
+            ),
             TextButton.icon(
               onPressed: () {},
-              label: Text("Reply"),
-              icon: Icon(FontAwesomeIcons.message),
+              label:
+                  Text("Reply", style: Theme.of(context).textTheme.titleMedium),
+              icon: Icon(FontAwesomeIcons.message,
+                  color: themeManager.themeMode == dark
+                      ? whitee
+                      : const Color.fromARGB(255, 58, 59, 60)),
             )
           ],
         ),
